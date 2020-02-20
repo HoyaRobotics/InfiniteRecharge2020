703bcbe772dc35fcf90f414e1aadc2a2cec836ad
Hoya Robotics <hoyarobotics@gmail.com>
2020-02-20 10:42:34 -0500

    Added helper class to access Limelight

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ import frc.robot.commands.DriveWithJoystick;
 import frc.robot.subsystems.DriveBase;
 import frc.robot.subsystems.Gearbox;
 import frc.robot.subsystems.Intake;
+import frc.robot.util.Limelight;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -17,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
 
   private final XboxController controller = new XboxController(CONTROLLER);
+  private final Limelight limelight = new Limelight();
 
   private final DriveBase driveBase = new DriveBase();
   private final Gearbox gearbox = new Gearbox();
