4edf61ea142b220a88e3fe6faa47e29e71a4a02f
Hoya Robotics <hoyarobotics@gmail.com>
2020-02-20 10:54:09 -0500

    Changed DriveBase to use DifferentialDrive

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -1,33 +1,31 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.util.Utils;
 
 import static frc.robot.Constants.*;
 
 public class DriveBase extends SubsystemBase {
+    
+    private final WPI_TalonFX leftMaster = new WPI_TalonFX(FRONT_LEFT_DRIVE);
+    private final WPI_TalonFX rightMaster = new WPI_TalonFX(FRONT_RIGHT_DRIVE);
+    private final WPI_TalonFX leftSlave = new WPI_TalonFX(REAR_LEFT_DRIVE);
+    private final WPI_TalonFX rightSlave = new WPI_TalonFX(REAR_RIGHT_DRIVE);
 
-    private final TalonFX frontLeftMotor = new TalonFX(FRONT_LEFT_DRIVE);
-    private final TalonFX frontRightMotor = new TalonFX(FRONT_RIGHT_DRIVE);
-    private final TalonFX rearLeftMotor = new TalonFX(REAR_LEFT_DRIVE);
-    private final TalonFX rearRightMotor = new TalonFX(REAR_RIGHT_DRIVE);
+    private final DifferentialDrive drive = new DifferentialDrive(leftMaster, rightMaster);
+
+    public DriveBase(){
+        leftSlave.follow(leftMaster);
+        rightSlave.follow(rightMaster);
+    }
 
     /**
      * Drives the robot with arcade controls, supplying throttle and rotation.
      */
     public void arcadeDrive(double throttle, double rotation){
-        rotation *= -1;
-
-        double leftSide = Utils.limit(throttle + rotation);
-        double rightSide = Utils.limit((throttle * -1) + rotation);
-
-        frontLeftMotor.set(ControlMode.PercentOutput, leftSide);
-        rearLeftMotor.set(ControlMode.PercentOutput, leftSide);
-        frontRightMotor.set(ControlMode.PercentOutput, rightSide);
-        rearRightMotor.set(ControlMode.PercentOutput, rightSide);
+        drive.arcadeDrive(throttle, rotation);
     }
 
 }
