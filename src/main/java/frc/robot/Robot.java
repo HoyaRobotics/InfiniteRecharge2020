9e379aabc9cf245aab1519faf8a9e82d73e3c0fc
Hoya Robotics <hoyarobotics@gmail.com>
2020-02-21 15:53:45 -0500

    Added loop time monitor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.util.Logger;
@@ -28,9 +29,11 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+    long start = System.currentTimeMillis();
     CommandScheduler.getInstance().run();
 
     Logger.flush();
+    SmartDashboard.putNumber("loopTime", System.currentTimeMillis() - start);
   }
 
   /**
