6adc6b54feb3729d727e3f1eab8f01c12d7a6a87
Hoya Robotics <hoyarobotics@gmail.com>
2020-02-24 09:04:41 -0500

    Added command to manually rotate turret

---
+++ src/main/java/frc/robot/commands/RotateWithJoystick.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import java.util.function.DoubleSupplier;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.Turret;
+import frc.robot.util.Utils;
+
+public class RotateWithJoystick extends CommandBase {
+
+    private final Turret turret;
+    private final DoubleSupplier input;
+
+    public RotateWithJoystick(Turret turret, DoubleSupplier input){
+        this.turret = turret;
+        this.input = input;
+
+        addRequirements(turret);
+    }
+
+    @Override
+    public void execute(){
+        turret.setRotatorSpeed(Utils.applyDeadband(input.getAsDouble(), Constants.CONTROL_DEADBAND) / 2);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        turret.setRotatorSpeed(0);
+    }
+
+}
