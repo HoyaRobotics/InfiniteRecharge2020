4bf512e9e548e930000a408e711ed2657f21dd54
Hoya Robotics <hoyarobotics@gmail.com>
2020-02-29 16:10:41 -0500

    Update ShootBall sequence

--- src/main/java/frc/robot/commands/ShootBall.java
+++ src/main/java/frc/robot/commands/ShootBall.java
@@ -1,7 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.TimedIntake.IntakeMode;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Turret;
@@ -9,15 +10,29 @@ import frc.robot.util.Limelight;
 
 public class ShootBall extends SequentialCommandGroup {
 
+    private final Shooter shooter;
+    private final Turret turret;
+    private final Intake intake;
+
     public ShootBall(Shooter shooter, Intake intake, Turret turret, Limelight limelight){
+        this.shooter = shooter;
+        this.turret = turret;
+        this.intake = intake;
+//mct work on feb 29
         addCommands(
-            new ParallelCommandGroup(new AlignTurret(turret, limelight), new ShooterRev(shooter, limelight)),
-            new ReleaseBall(intake, shooter)
+            new AlignTurret(turret, limelight),
+            new ShooterRev(shooter, limelight),
+            new TimedIntake(intake, -1.0, 100, IntakeMode.INTERNAL),
+            new InstantCommand(() -> shooter.openBallGate()),
+            new TimedIntake(intake, 1.0, 1000, IntakeMode.INTERNAL),
+            new InstantCommand(() -> shooter.closeBallGate())
         );
     }
 
     @Override
     public void end(boolean interrupted){
-        shooter.setFlywheelRPM(0);
+        shooter.setFlywheelPercent(0);
+        turret.setRotatorSpeed(0);
+        intake.setInternalRoller(0);
     }
 }
