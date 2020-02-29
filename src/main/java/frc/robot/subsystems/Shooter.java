b9b38f7e3dce19a157560875d3d45381eb9886e8
Hoya Robotics <hoyarobotics@gmail.com>
2020-02-29 16:13:13 -0500

    Updated Shooter with PID

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.Logger;
 
 import static frc.robot.Constants.*;
 
@@ -15,25 +17,31 @@ public class Shooter extends SubsystemBase {
 
     private final Solenoid ballGate = new Solenoid(BALL_GATE);
 
-    private final CANSparkMax master = new CANSparkMax(SHOOTER_LEFT, MotorType.kBrushless);
-    private final CANSparkMax slave = new CANSparkMax(SHOOTER_RIGHT, MotorType.kBrushless);
+    private final CANSparkMax left = new CANSparkMax(SHOOTER_LEFT, MotorType.kBrushless);
+    private final CANSparkMax right = new CANSparkMax(SHOOTER_RIGHT, MotorType.kBrushless);
 
-    private final CANPIDController pid = master.getPIDController();
-    private final CANEncoder encoder = master.getEncoder();
+    private final CANPIDController pid = right.getPIDController();
+    private final CANEncoder encoder = right.getEncoder();
 
     private double[] rpmLog = new double[20];
     private int rpmLogCounter = 0;
 
-    public Shooter(){
-        pid.setFeedbackDevice(encoder);
-        pid.setOutputRange(-1, 1);
-        pid.setP(SHOOTER_P);
-        pid.setI(SHOOTER_I);
-        pid.setD(SHOOTER_D);
-        pid.setIZone(SHOOTER_IZ);
-        pid.setFF(SHOOTER_FF);
+    private boolean gateOpen;
 
-        slave.follow(master);
+    public Shooter(){
+        left.restoreFactoryDefaults();
+        right.restoreFactoryDefaults();
+
+        right.setOpenLoopRampRate(1);
+        right.setClosedLoopRampRate(1);
+
+        pid.setFeedbackDevice(encoder);
+        pid.setFF(0.00019);
+        pid.setP(0.0003);
+        
+        left.follow(right, true);
+
+        closeBallGate();
     }
 
     @Override
@@ -44,33 +52,69 @@ public class Shooter extends SubsystemBase {
             rpmLogCounter++;
         else
             rpmLogCounter = 0;
+
+        SmartDashboard.putNumber("flywheelRPM", encoder.getVelocity());
     }
 
-    public boolean isStableAt(double rpm){
-        double average = 0;
+    public boolean isStable(){
+        double highest = Double.MIN_VALUE;
+        double lowest = Double.MAX_VALUE;
 
-        for(int i = 0; i < rpmLog.length; i++)
-            average += rpmLog[i];
+        double sum = 0;
+        double nonZero = 0;
+        for(int i = 0; i < rpmLog.length; i++){
+            double thisRPM = rpmLog[i];
+            if(thisRPM == 0)
+                continue;
 
-        average /= rpmLog.length;
+            nonZero++;
+            if(thisRPM < lowest)
+                lowest = thisRPM;
+            if(thisRPM > highest)
+                highest = thisRPM;
+            sum += thisRPM;
+        }
 
-        return Math.abs(rpm - average) < RPM_STABILITY_ERROR;
+        if(nonZero < 10)
+            return false;
+
+        double average = sum / nonZero;
+
+        return (highest - average) < RPM_STABILITY_ERROR &&
+                (average - lowest) < RPM_STABILITY_ERROR;
     }
 
     public void setFlywheelRPM(double rpm){
         pid.setReference(rpm, ControlType.kVelocity);
     }
 
+    public void setFlywheelPercent(double percent){
+        right.set(percent);
+    }
+
     public double getFlywheelRPM(){
         return encoder.getVelocity();
     }
 
+    public void toggleBallGate(){
+        if(gateOpen){
+            closeBallGate();
+            Logger.info("Ball gate opened");
+        }else{
+            openBallGate();
+            Logger.info("Ball gate closed");
+            
+        }
+    }
+
     public void openBallGate(){
         ballGate.set(true);
+        gateOpen = true;
     }
 
     public void closeBallGate(){
         ballGate.set(false);
+        gateOpen = false;
     }
 
 }
